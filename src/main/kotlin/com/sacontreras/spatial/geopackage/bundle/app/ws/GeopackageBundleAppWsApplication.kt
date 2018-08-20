a8bf50237e6f8f7ce9222568d32cab06e1f089db
Steven Contreras <steven@terranodo.io>
2018-08-20 13:20:14 -0700

    initial maven deps - embedded tomcat starts

---
+++ src/main/kotlin/com/sacontreras/spatial/geopackage/bundle/app/ws/GeopackageBundleAppWsApplication.kt
@@ -0,0 +1,11 @@
+package com.sacontreras.spatial.geopackage.bundle.app.ws
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GeopackageBundleAppWsApplication
+
+fun main(args: Array<String>) {
+    runApplication<GeopackageBundleAppWsApplication>(*args)
+}
